a7534ff8f2a2539c83fc9b8dd586440b548aa285
Sallust <daniyom@gmail.com>
2016-04-15 14:09:33 -0400

    refactor: data structure and for loop set up in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,53 @@
 //app.js
+var projectsArray = [
+	{
+		image: 'img/nutrition-tracker-darker.png',
+		title: 'Nutrition Tracker Single-Page App',
+		skills: 'HTML, Backbone,'
+	},
+	{
+		image: 'img/nutrition-tracker-darker.png',
+		title: 'Nutrition Tracker Single-Page App',
+		skills: 'HTML, Backbone,'
+	},
+	{
+		image: 'img/nutrition-tracker-darker.png',
+		title: 'Nutrition Tracker Single-Page App',
+		skills: 'HTML, Backbone,'
+	},
+	{
+		image: 'img/nutrition-tracker-darker.png',
+		title: 'Nutrition Tracker Single-Page App',
+		skills: 'HTML, Backbone,'
+	},
+	{
+		image: 'img/nutrition-tracker-darker.png',
+		title: 'Nutrition Tracker Single-Page App',
+		skills: 'HTML, Backbone,'
+	},
+	{
+		image: 'img/nutrition-tracker-darker.png',
+		title: 'Nutrition Tracker Single-Page App',
+		skills: 'HTML, Backbone,'
+	}
+];
+
+
+
 
 var container = $('#feature-container');
 
-var template = _.template($('#test-template').html() );
+var template = _.template($('#tile-template').html() );
+/**
+* @description Iterates over array of project obj, plugs into template, appends to page
+* 
+*/
 
-var templateHTML = template({testString:'Sup World'});
+_.each(projectsArray, function(project) {
+	templateHTML = template(project);
+	container.append(templateHTML);
+} );
 
-container.append(templateHTML);
+
 
 
